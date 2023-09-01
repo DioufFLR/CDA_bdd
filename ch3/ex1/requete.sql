6bb3b7a84d9ea07e212aaa7bab9413d61ccd4aea
diouf <geoffreyfleur@gmail.com>
2023-09-01 10:58:22 +0200

    👌 IMPROVE: exercises requete sql

--- ch3/ex1/requete.sql
+++ ch3/ex1/requete.sql
@@ -16,3 +16,34 @@ SELECT titre FROM employe;
 
 -- 5
 
+SELECT DISTINCT titre FROM employe;
+
+-- 6
+
+SELECT nom, noemp, nodep FROM employe
+WHERE titre = 'secrétaire';
+
+-- 7
+
+SELECT nom, nodept FROM dept
+WHERE nodept > 40;
+
+-- 8
+
+SELECT nom, prenom FROM employe WHERE nom < prenom;
+
+-- 9
+
+SELECT employe.nom, employe.salaire, employe.nodep FROM employe
+WHERE employe.titre = 'représentant' 
+AND employe.nodep = '35' 
+AND employe.salaire > 20000;
+
+-- 10
+
+SELECT nom, titre, salaire FROM employe
+WHERE employe.titre = 'représentant'
+OR employe.titre = 'président';
+
+-- 11
+
