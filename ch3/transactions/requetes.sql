d6369aaa267b2d18e37346f0e9be7d91537edca4
diouf <geoffreyfleur@gmail.com>
2023-09-12 09:30:02 +0200

    :construction: papyrus exercises stocked process

---
+++ ch3/transactions/requetes.sql
@@ -0,0 +1,13 @@
+-- 1
+
+START TRANSACTION;
+
+SELECT nomfou FROM fournis WHERE numfou=120;
+
+UPDATE fournis  SET nomfou= 'GROSBRIGAND' WHERE numfou=120
+
+SELECT nomfou FROM fournis WHERE numfou=120
+
+
+select nomfou from fournis
+where numfou= 120
