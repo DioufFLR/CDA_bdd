b362314215e185e9e8ac74459be4f3f9da49f302
diouf <geoffreyfleur@gmail.com>
2023-09-12 16:50:44 +0200

    :construction: hotel trigger

--- ch3/declencheurs/cas_pratique/requetes.sql
+++ ch3/declencheurs/cas_pratique/requetes.sql
@@ -31,6 +31,17 @@ update lignedecommande
 set prix = 1222
 where id_produit = 2;
 
+-- Pour delete
+
+CREATE TRIGGER maj_total_delete AFTER UPDATE ON lignedecommande
+    FOR EACH ROW
+BEGIN
+    DECLARE id_c INT;
+    DECLARE tot DOUBLE;
+    SET id_c = NEW.id_commande; -- nous captons le numéro de commande concerné
+    SET tot = (SELECT sum(prix*quantite) FROM lignedecommande WHERE id_commande=id_c); -- on recalcul le total
+    UPDATE commande SET total=tot WHERE id=id_c; -- on stocke le total dans la table commande
+END;
 
 -- 3
 
