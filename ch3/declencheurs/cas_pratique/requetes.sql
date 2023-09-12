bf2022a87f6e9f01acaf3213d0c87c3baf852b03
diouf <geoffreyfleur@gmail.com>
2023-09-12 16:33:30 +0200

    :construction: hotel trigger

---
+++ ch3/declencheurs/cas_pratique/requetes.sql
@@ -0,0 +1,36 @@
+-- 1
+
+CREATE TRIGGER maj_total AFTER INSERT ON lignedecommande
+    FOR EACH ROW
+BEGIN
+    DECLARE id_c INT;
+    DECLARE tot DOUBLE;
+    SET id_c = NEW.id_commande; -- nous captons le numéro de commande concerné
+    SET tot = (SELECT sum(prix*quantite) FROM lignedecommande WHERE id_commande=id_c); -- on recalcul le total
+    UPDATE commande SET total=tot WHERE id=id_c; -- on stocke le total dans la table commande
+END;
+
+-- on vérifie si le trigger fonctionne bien
+insert into lignedecommande(id_commande, id_produit, quantite, prix) VALUES (1, 2, 23, 145);
+
+-- 2
+
+CREATE TRIGGER maj_total_update AFTER UPDATE ON lignedecommande
+    FOR EACH ROW
+BEGIN
+    DECLARE id_c INT;
+    DECLARE tot DOUBLE;
+    SET id_c = NEW.id_commande; -- nous captons le numéro de commande concerné
+    SET tot = (SELECT sum(prix*quantite) FROM lignedecommande WHERE id_commande=id_c); -- on recalcul le total
+    UPDATE commande SET total=tot WHERE id=id_c; -- on stocke le total dans la table commande
+END;
+
+-- on modifie le trigger pour modifier un produit et mettre à jour le total
+
+update lignedecommande
+set prix = 1222
+where id_produit = 2;
+
+
+-- 3
+
